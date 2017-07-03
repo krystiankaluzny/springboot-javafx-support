262168572d4d3fb119542f820b36aff9acb6122b
Krystian Kałużny <k.kaluzny141@gmail.com>
2017-07-03 14:39:44 +0200

    Fix existing tests

--- src/test/java/jfxtest/annotated/AnnotatedApp.java
+++ src/test/java/jfxtest/annotated/AnnotatedApp.java
@@ -3,11 +3,12 @@ package jfxtest.annotated;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 import de.felixroske.jfxsupport.AbstractJavaFxApplicationSupport;
+import de.felixroske.jfxsupport.JavaFxSupport;
 
 @SpringBootApplication
 public class AnnotatedApp extends AbstractJavaFxApplicationSupport {
 
 	public static void main(String[] args) {
-		launchApp(AnnotatedApp.class, AnnotatedView.class, args);
+		JavaFxSupport.launchApp(AnnotatedApp.class, AnnotatedView.class, args);
 	}
 }
--- src/test/java/jfxtest/plain/PlainApp.java
+++ src/test/java/jfxtest/plain/PlainApp.java
@@ -3,11 +3,12 @@ package jfxtest.plain;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 import de.felixroske.jfxsupport.AbstractJavaFxApplicationSupport;
+import de.felixroske.jfxsupport.JavaFxSupport;
 
 @SpringBootApplication
 public class PlainApp extends AbstractJavaFxApplicationSupport {
 
 	public static void main(String[] args) {
-		launchApp(PlainApp.class, PlainView.class, args);
+		JavaFxSupport.launchApp(PlainApp.class, PlainView.class, null, args);
 	}
 }
